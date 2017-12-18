9f54b5b9335f51d9515d8401c5472c8f8603b272
Brian Tu <feihcsim824@gmail.com>
2017-12-18 15:19:34 -0500

    adjust public path for non-root project

--- config/index.js
+++ config/index.js
@@ -56,7 +56,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/long-tsla/dist/',
 
     /**
      * Source Maps
