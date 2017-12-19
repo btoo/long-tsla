a184a52c5275a25d89a93513a8a172bd28a3967a
Brian Tu <feihcsim824@gmail.com>
2017-12-18 22:02:42 -0500

    close button for returning home

---
+++ src/components/CloseButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <router-link to="/" />
+</template>
+
+<script>
+export default {
+  name: 'CloseButton',
+  props: ['callback']
+}
+</script>
+
+<style lang="scss" scoped>
+  a {
+    color: #000;
+    position: relative;
+    margin-top: 0;
+    margin-left: 0;
+    width: 21px;
+    height: 21px;
+
+    &:before, &:after {
+      content: '';
+      position: absolute;
+      top: 10px;
+      left: 0;
+      width: 21px;
+      height: 1px;
+      background-color: currentColor;
+    }
+
+    &:before {
+      transform: rotate(-45deg);
+    }
+
+    &:after {
+      transform: rotate(45deg);
+    }
+  }
+</style>
