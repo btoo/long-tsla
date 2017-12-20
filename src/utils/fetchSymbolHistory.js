99526dcfbf2a3fa7c0d2b1000d4d4bfb7ba16040
Brian Tu <feihcsim824@gmail.com>
2017-12-19 20:45:56 -0500

    async fn clean up

--- src/utils/fetchSymbolHistory.js
+++ src/utils/fetchSymbolHistory.js
@@ -1,6 +1,6 @@
 import NProgress from 'NProgress'
 
-export const fetchSymbolHistory = apiKey => async function (symbol, callback) {
+export const fetchSymbolHistory = apiKey => async (symbol, callback) => {
   const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=full&apikey=${apiKey}`)
   const raw = await response.json()
 
